bcf6a24bebbe81e3f49424ec03a56d6ddfb4df9f
Mansour J. Mansour <mansour@mjmonline.se>
2017-07-09 22:56:23 +0200

    creates floatlabel module

--- app/modules/floatlabel/floatlabel.js
+++ app/modules/floatlabel/floatlabel.js
@@ -1,10 +1,45 @@
 class FloatLabel {
     constructor() {
-        this.className = '.float-label-wrapper';
+        this.populatedClass = 'populated';
+        this.focusedClass = 'focused';
+        this.className = 'float-label-wrapper';
+    };
+
+    isFieldFilled(field) {
+        return (field.value !== null && field.value !== '');
     };
 
     init() {
-        $(this.className).FloatLabel();
+        let self = this;
+        let fields = Array.from(document.getElementsByClassName(this.className));
+
+        fields.forEach(function(field, i) {
+            let inputs = Array.from(field.querySelectorAll('textarea, input'));
+
+            inputs.forEach(input => {
+                if(self.isFieldFilled(input)) {
+                    field.classList.add(self.populatedClass);
+                }
+
+                input.addEventListener('focus', function() {
+                    input.classList.add(self.focusedClass);
+                    if(self.isFieldFilled(input)) {
+                        field.classList.add(self.populatedClass);
+                    }
+                });
+
+                input.addEventListener('blur', function() {
+                    input.classList.remove(self.focusedClass);
+                    if(input.value === '') {
+                        field.classList.remove(self.populatedClass);
+                    }
+                });
+
+                input.addEventListener('keyup', function() {
+                    field.classList.add(self.populatedClass);
+                });
+            });
+        });
     };
 };
 
